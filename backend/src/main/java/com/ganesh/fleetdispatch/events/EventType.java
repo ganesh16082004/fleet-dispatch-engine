a505311d57ee6b6f70eae8b8b0f4c9fb2791961d
ganesh16082004 <poornasimha@gmail.com>
2026-09-03 18:17:09 +0530

    feat: define core event types

---
+++ backend/src/main/java/com/ganesh/fleetdispatch/events/EventType.java
@@ -0,0 +1,12 @@
+package com.ganesh.fleetdispatch.events;
+
+public enum EventType {
+    ORDER_CREATED,
+    RESTAURANT_READY,
+    RIDER_LOCATION_UPDATED,
+    RIDER_AVAILABLE,
+    RIDER_ASSIGNED,
+    ORDER_PICKED_UP,
+    ORDER_DELIVERED,
+    ORDER_CANCELLED
+}
