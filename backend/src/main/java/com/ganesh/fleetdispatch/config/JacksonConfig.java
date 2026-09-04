08aaf84bba6254024f99e5b017ec80a24fd141c8
ganesh16082004 <poornasimha@gmail.com>
2026-09-04 23:14:16 +0530

    Configure Jackson modules for Kafka event serialization

--- backend/src/main/java/com/ganesh/fleetdispatch/config/JacksonConfig.java
+++ backend/src/main/java/com/ganesh/fleetdispatch/config/JacksonConfig.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper().findAndRegisterModules();
     }
 }
